bc59bfeaf89107bac2aedf397cb2a7499da7b799
Toy Bonaparte Chan <jackiebonaparte@gmail.com>
2023-05-25 22:19:44 +0800

    Update 10.2editconfig_project_setting.py

--- 10/10.2editconfig_project_setting.py
+++ 10/10.2editconfig_project_setting.py
@@ -9,22 +9,22 @@ import matplotlib as mpl
 mpl.matplotlib_fname()
 
 # 抑制所有报错
-import warnings
-warnings.filterwarnings("ignore")
+# import warnings
+# warnings.filterwarnings("ignore")
 
 # normal plot 正常
 plt.axes([0.1, 0.7, 0.3, 0.3], frameon=True, facecolor="y", aspect="equal")  # 图位置，轴脊，背景颜色
 plt.plot(np.arange(3), [0.1, 0])
-plt.cla()
-plt.plot(np.arange(3), [0.1, 0])
+# plt.cla()
+# plt.plot(np.arange(3), [0.1, 0])
 
 # no plot
 plt.axes([0.4, 0.4, 0.3, 0.3], frameon=True, facecolor="y", aspect="equal")  # 图位置，轴脊，背景颜色
-plt.plot(2 + np.arange(3), [0.1, 0])
+plt.plot(2+np.arange(3), [0.1, 0])
 
 # no axes 无轴
 plt.axes([0.7, 0.1, 0.3, 0.3], frameon=True, facecolor="y", aspect="equal")  # 图位置，轴脊，背景颜色
-plt.plot(4 + np.arange(3), [0.1, 0])
+plt.plot(4+np.arange(3), [0.1, 0])
 plt.axis("off")
 
 plt.show()
